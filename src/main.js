d9048b057753519ec243041f8b4bd2d9f7a4b557
Graeme Woods <graemewoods202@gmail.com>
2021-05-26 12:30:58 -0300

    add fontawesome

--- src/main.js
+++ src/main.js
@@ -4,6 +4,15 @@ import Vue from 'vue'
 import './plugins/bootstrap-vue'
 import App from './App.vue'
 
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+import { faCopy } from '@fortawesome/free-solid-svg-icons'
+library.add(faCopy)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+
 Vue.config.productionTip = false
 
 new Vue({
