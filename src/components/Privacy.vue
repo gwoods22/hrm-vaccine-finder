b15eba7a3b1b64fbcb507d69d7572253112dfcd9
Graeme Woods <graemewoods202@gmail.com>
2021-05-26 10:00:48 -0300

    add Privacy Policy component

---
+++ src/components/Privacy.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="terms container">
+    <h1>The World's Greatest Privacy Policy</h1>
+    <p>We don't store your data, period.</p>
+    <p>We physically can't, because we don't collect it. We're here to show you data, specifically vaccine appointments near you.</p>
+    <p>We never store the address you type in, not even temporarily. It's only used to find out how far you are from appointment locations and then it's immediately lost forever. We don't even have a server database to store it.</p>
+  </div>
+</template>
