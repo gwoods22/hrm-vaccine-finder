c1de29a18fede7482cb0484e790a7fc400832461
Graeme Woods <graemewoods202@gmail.com>
2021-11-12 16:51:09 -0500

    Update privacy policy

--- src/views/Privacy.vue
+++ src/views/Privacy.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="container">
     <h1>HRM Vaccine Appointments</h1>
-    <h3>The World's Greatest Privacy Policy</h3>
-    <p>We don't store your data, period.</p>
-    <p>We physically can't, because we don't collect it. We're here to show you data, specifically vaccine appointments near you.</p>
-    <p>We never store the address you type in, not even temporarily. It's only used to find out how far you are from appointment locations and then it's immediately lost forever. We don't even have a server database to store it.</p>
+    <h4>The World's Greatest Privacy Policy</h4>
+    <p>We don't store your data, <span class="font-weight-bold">period</span>.</p>
+    <p>
+      We physically can't, because we don't collect it. We don't use
+      cross-site trackers, we don't run ads and we never will.
+    </p>
+    <p>We're here to show you information and nothing else.</p>
+    <!-- <p>We never store the address you type in, not even temporarily. It's only used to find out how far you are from appointment locations and then it's immediately lost forever. We don't even have a server database to store it.</p> -->
     <router-link to="/">Back Home</router-link> 
   </div>
 </template>
